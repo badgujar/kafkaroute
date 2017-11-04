afc359253f3ca34034fc37f0c3a3eef309707593
Amol Badgujar <badgujaramol@gmail.com>
2017-11-04 18:42:17 +0530

    checked

---
+++ src/main/java/ai/Application.java
@@ -0,0 +1,18 @@
+package ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by hua.ai on 2016/11/5.
+ * Main entrance for spring boot application
+ *
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/main/java/ai/PreProcessor.java
@@ -0,0 +1,10 @@
+package ai;
+
+public class PreProcessor {
+	
+	public String doTransform(String content2Enrich){
+		System.out.println("displaying content2Enrich  ::  " + content2Enrich);
+		return content2Enrich;
+	}
+
+}
